88974869386506e616784db0b38c0b60e723caf7
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2023-11-29 09:38:05 +0330

    app js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,62 +1,25 @@
-const fruitStates = {
-    apple: 5,
-    orange: 10
-}
-const foodStates = {
-    sandwich: 15
-}
+const states = {
+  apple: 5,
+};
 
-const fruitReducer = (state = fruitStates , action )=>{
-    switch (action.type) {
-        case "BUY_APPLE":
-            return {...state , apple: state.apple -1}
-        case "BUY_ORANGE":
-            return {...state , orange: state.orange -1}
-        default:
-            return state
-    }
-}
+const reducer = (state = states, action) => {
+  switch (action.type) {
+    case "Buy_Apple":
+      return { apple: state.apple - 1 };
+    default:
+      return state;
+  }
+};
 
-const foodReducer = (state = foodStates , action )=>{
-    switch (action.type) {
-        case "BUY_SANDWICH":
-            return {...state , sandwich: state.sandwich -1}
-        default:
-            return state
-    }
-}
+const store = Redux.createStore(reducer);
 
-const rootReducer = Redux.combineReducers({
-    fruit: fruitReducer,
-    food: foodReducer
-})
+store.subscribe(() => {
+  document.getElementById("apple-count").innerHTML =
+    "apple number:" + store.getState().apple;
+});
+document.getElementById("buy-apple").addEventListener("click", () => {
+  store.dicpatch({ type: "Buy_Apple" });
+});
 
-const store = Redux.createStore(rootReducer);
-
-const render = ()=>{
-    console.log(store.getState());
-    document.getElementById('apple_count').innerHTML = "تعداد سیب ها :" + store.getState().fruit.apple
-    document.getElementById('orange_count').innerHTML = "تعداد پرتقال ها :" + store.getState().fruit.orange
-    document.getElementById('sandwich_count').innerHTML = "تعداد ساندوسچ ها :" + store.getState().food.sandwich
-}
-
-store.subscribe(()=>{
-    render()
-})
-
-document.getElementById('buy_apple').addEventListener('click' , ()=>{
-    store.dispatch({type:"BUY_APPLE"})
-})
-document.getElementById('buy_orange').addEventListener('click' , ()=>{
-    store.dispatch({type:"BUY_ORANGE"})
-})
-document.getElementById('buy_sandwich').addEventListener('click' , ()=>{
-    store.dispatch({type:"BUY_SANDWICH"})
-})
-
-render()
-
-
-
-
-
+document.getElementById("apple-count").innerHTML =
+  "apple number:" + store.getState().apple;
