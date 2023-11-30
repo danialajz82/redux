4af583677e20f786e1ee17c9d3556d64c62ce761
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2023-11-30 14:47:32 +0330

    redux-shopping

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,58 @@
-const states = {
+const fruitStates = {
   apple: 5,
+  orange: 10,
+};
+const foodStates = {
+  sandwich: 15,
 };
 
-const reducer = (state = states, action) => {
+const fruitReducer = (state = fruitStates, action) => {
   switch (action.type) {
     case "Buy_Apple":
-      return { apple: state.apple - 1 };
+      return { ...state, apple: state.apple - 1 };
+    case "Buy_Orange":
+      return { ...state, orange: state.orange - 1 };
     default:
       return state;
   }
 };
 
-const store = Redux.createStore(reducer);
+const foodReducer = (state = foodStates, action) => {
+  switch (action.type) {
+    case "Buy_Sandwich":
+      return { ...state, sandwich: state.sandwich - 1 };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = Redux.combineReducers({
+  fruit : fruitReducer, 
+  food: foodReducer
+})
+const store = Redux.createStore(rootReducer);
+
+const render = () => {
+  document.getElementById("apple-count").innerHTML =
+    "تعداد سیب ها" + store.getState().fruit.apple;
+  document.getElementById("orange-count").innerText =
+    "تعداد پرتغال ها" + store.getState().fruit.orange;
+    document.getElementById("sandwich-count").innerText =
+    "تعداد ساندویج ها" + store.getState().food.sandwich;
+};
 
 store.subscribe(() => {
-  document.getElementById("apple-count").innerHTML =
-    "apple number:" + store.getState().apple;
-});
-document.getElementById("buy-apple").addEventListener("click", () => {
-  store.dicpatch({ type: "Buy_Apple" });
+  render();
 });
 
-document.getElementById("apple-count").innerHTML =
-  "apple number:" + store.getState().apple;
+document.getElementById("buy-apple").addEventListener("click", () => {
+  store.dispatch({ type: "Buy_Apple" });
+});
+document.getElementById("buy-orange").addEventListener("click", () => {
+  store.dispatch({ type: "Buy_Orange" });
+});
+document.getElementById("buy-sandwich").addEventListener("click", () => {
+  store.dispatch({ type: "Buy_Sandwich" });
+});
+
+render();
